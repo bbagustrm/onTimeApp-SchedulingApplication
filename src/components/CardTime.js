770f213b2d6f8464bf5a64310bd92ee2e9497237
Bagus Tri Atmojo <bbagustrm@gmail.com>
2024-11-18 18:42:14 +0700

    delete fitur auto reset

--- src/components/CardTime.js
+++ src/components/CardTime.js
@@ -312,40 +312,6 @@ function CardTime() {
     
         return currentTime > endTime; // Periksa apakah waktu sekarang sudah lewat dari endTime
     }
-
-    useEffect(() => {
-        const updateStatuses = async () => {
-            try {
-                // Lakukan pengecekan untuk setiap item
-                const updatedTimes = await Promise.all(
-                    times.map(async (time) => {
-                        if (!time.status && isTimePassed(time.end_jam)) {
-                            // Perbarui status di server
-                            await fetch(`https://restapi-3bfde-default-rtdb.firebaseio.com/time/${time.id}.json${token}`, {
-                                method: "PATCH",
-                                body: JSON.stringify({ status: true }),
-                            });
-    
-                            // Kembalikan objek dengan status yang diubah
-                            return { ...time, status: true };
-                        }
-                        return time; // Jika tidak lewat, kembalikan objek asli
-                    })
-                );
-    
-                setTimes(updatedTimes); // Perbarui state
-            } catch (err) {
-                console.error("Gagal memperbarui status otomatis:", err);
-            }
-        };
-    
-
-        const interval = setInterval(updateStatuses, 60 * 10000);
-        updateStatuses();
-    
-        return () => clearInterval(interval); // Hentikan interval saat komponen unmount
-    }, [times, token]);
-    
     
 
     if (loading) return <p className="text-center">Loading...</p>;
